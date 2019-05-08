7a5e7486aea1faccf6cde410f0de6a20f8320fa8
LittleControl <littlecontrol@qq.com>
2019-05-08 21:15:21 +0800

    add Node folder

---
+++ Code/Web/Node/new01/Module.js
@@ -0,0 +1,8 @@
+console.log("I'm Module.js!");
+exports.x="666";
+// console.log(arguments.callee+"");
+// module.exports.x="666";
+module.exports={
+    name:"LittleControl",
+    age:18
+}
---
+++ Code/Web/Node/new01/math.js
@@ -0,0 +1,7 @@
+console.log("this is a math function 仓库");
+exports.add=function(a,b){
+    console.log(a+b);
+}
+exports.mul=function(a,b){
+    console.log(a*b);
+}
