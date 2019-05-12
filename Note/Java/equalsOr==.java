dc6688ebeed7c5f95461957a92d3d90c7a1f16f3
LittleControl <littlecontrol@qq.com>
2019-05-12 23:01:31 +0800

    补漏

---
+++ Note/Java/equalsOr==.java
@@ -0,0 +1,17 @@
+package Java;
+
+class Test {
+    public static void main(String[] args) {
+        String str1="LittleControl";
+        String str2="LittleControl";
+        System.out.println(str1==str2);//true
+        System.out.println(str1.equals(str2));//true
+        String str3=new String("LittleControl");
+        String str4=new String("LittleControl");
+        System.out.println(str3==str4);//false
+        System.out.println(str3.equals(str4));//true
+    }
+}
+/*烟火里的尘埃
+今朝有酒今朝醉，明日散发弄扁舟
+这个城市的风很大，孤独的人总是晚回家*/
