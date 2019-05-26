7e07a44b423f5ef86d79e6b1f8d5ec0e36c5768a
LittleControl <littlecontrol@qq.com>
2019-05-26 21:24:06 +0800

    CRUD全部完成,并且加入了边界情况的检测

--- Code/Java/Leagues/src/top/littlecontrol/Operation.java
+++ Code/Java/Leagues/src/top/littlecontrol/Operation.java
@@ -1,23 +1,93 @@
 package top.littlecontrol;
 
+import java.util.Scanner;
+
 public class Operation {
-	public void add(int id1,int id0,Member[] mem,Army[] arr_Army) {
-		mem[id1]=new Member(id1, id0, arr_Army[id0].name, arr_Army[id0].career, arr_Army[id0].money);
-		arr_Army[id0].status=false;
-	}
-	public void del(int id1,Member[] mem,Army[] arr_Army) {
+	public void Del(int id1,Member[] mem,Army[] arr_Army) {
 		arr_Army[mem[id1].id0].status=true;
 		for(int i=id1;i<4;i++) {
 			mem[id1]=mem[id1+1];
 		}
 	}
-	public void update(int id1,Member[] mem) {
-		
+	public void Update(int id1,Member[] mem) {
+		Scanner read=new Scanner(System.in);
+		System.out.println("请输入您要修改的姓名");
+		String name=read.next();
+		System.out.println("请输入您要修改的职业");
+		String career=read.next();
+		System.out.println("请输入您要修改的佣金");
+		int money=read.nextInt();
+		mem[id1].name=name;
+		mem[id1].career=career;
+		mem[id1].money=money;
 	}
-	public void Print_Member(Member[] mem) {
-		for(int i=0;i<mem.length;i++) {
-			System.out.println("编号\t姓名\t职业\t佣金\t");
+	public void Print_Member(Member[] mem,int count) {
+		System.out.println("编号\t姓名\t职业\t佣金\t");
+		for(int i=0;i<count;i++) {
 			System.out.println(mem[i].id1+"("+mem[i].id0+")"+"\t"+mem[i].name+"\t"+mem[i].career+"\t"+mem[i].money+"\t");
 		}
 	}
+	public int Add(int id1,int id0,Member[] mem,Army[] arr_Army) {
+		int result=0;
+		if(id1>=5) {
+			System.out.println("出战成员已满,不可添加");
+			result=-1;
+		}else if(arr_Army[id0].status==false) {
+			System.out.println("该成员已经出战,不可继续添加!");
+			result=-1;
+		}else {
+			int tag=Check(id1, mem);
+			if(tag==1&&arr_Army[id0].career.equals("法师")){
+				System.out.println("法师人数已达上限,无法添加");
+				result=-1;
+			}else if(tag==2&&arr_Army[id0].career.equals("射手")) {
+				System.out.println("射手人数已达上限,无法添加");
+				result=-1;
+			}else if(tag==3&&(arr_Army[id0].career.equals("法师")||arr_Army[id0].career.equals("射手"))) {
+				System.out.println("法师和射手的人数已达上限,无法添加");
+				result=-1;
+			}else if((id1==3)&&tag==0&&(!(arr_Army[id0].career.equals("法师")||arr_Army[id0].career.equals("射手")))) {
+				System.out.println("法师或射手的人数不够,请添加法师或射手");
+				result=-1;
+			}else if(id1==4&&tag==1&&(!arr_Army[id0].career.equals("射手"))) {
+				System.out.println("射手的人数不够,请添加射手");
+				result=-1;
+			}else if(id1==4&&tag==2&&(!arr_Army[id0].career.equals("法师"))) {
+				System.out.println("法师的人数不够,请添加法师");
+				result=-1;
+			}else {
+				arr_Army[id0].status=false;
+				mem[id1]=new Member(id1, id0, arr_Army[id0].name, arr_Army[id0].career, arr_Army[id0].money);
+			}
+		}
+		return result;
+	}
+	private int Check(int id1,Member[] mem) {
+		String name1="法师";
+		String name2="射手";
+		int tag1=0;
+		int tag2=0;
+		for(int i=0;i<id1;i++) {
+			if(mem[i].career.equals(name1)) {
+				tag1=1;
+			}
+			if(mem[i].career.equals(name2)) {
+				tag2=1;
+			}
+		}
+		if(tag1==1&&tag2==0) {
+			return 1;//射手不足
+		}else if(tag1==0&&tag2==1) {
+			return 2;//法师不足
+		}else if(tag1==1&&tag2==1) {
+			return 3;//法师与射手均足
+		}else {
+			return 0;//法师与射手均不足
+		}
+	}
 }
+
+
+
+
+
--- Code/Java/Leagues/src/top/littlecontrol/View.java
+++ Code/Java/Leagues/src/top/littlecontrol/View.java
@@ -5,12 +5,64 @@
  */
 package top.littlecontrol;
 
+import java.util.Scanner;
+
 public class View {
 	public static void main(String[] args) {
 		View view=new View();
 		Army[] arr_Army=new Army[10];
 		view.Init_Army(arr_Army);
 		view.Print_Army(arr_Army);
+		Member[] mem=new Member[5];
+		Operation op1=new Operation();
+		Scanner read=new Scanner(System.in);
+		System.out.println("请做出你的选择,-1表示退出");
+		System.out.println("1.增加出战成员 2.删除出战成员 3.修改出战成员 4.查看出战成员 5.查看军团成员");
+		/* count 用来计数member中的成员个数*/
+		int count=0;
+		/* id1是member的组内序号,默认与count保持一致*/
+		int id1=count;
+		int choice=read.nextInt();
+		while(choice!=-1) {
+			if(choice==1) {
+				System.out.println("请输入你要增加的出战成员的原始ID");
+				int id0=read.nextInt();
+				id1=count;
+				int result=op1.Add(id1, id0, mem, arr_Army);
+				if(result==0) {
+					count++;
+					id1++;
+				}
+			}else if(choice==2) {
+				System.out.println("请输入你要删除的出战成员的组内ID");
+				id1=read.nextInt();
+				if(id1>=5) {
+					System.out.println("你的输入有误,输入的数不应该大于4");
+				}else {
+					op1.Del(id1, mem, arr_Army);
+					count--;
+				}
+			}else if(choice==3) {
+				System.out.println("请输入你要修改的出站成员的组内ID");
+				if(id1>=5) {
+					System.out.println("你的输入有误,输入的数不应该大于4");
+				}else {
+					id1=read.nextInt();
+					op1.Update(id1, mem);
+				}
+			}else if(choice==4) {
+				op1.Print_Member(mem,count);
+			}else if(choice==5) {
+				view.Print_Army(arr_Army);
+			}else {
+				System.out.println("你的输入有误,请重新输入!");
+			}
+			System.out.println("请做出你的选择,-1表示退出");
+			System.out.println("1.增加出战成员 2.删除出战成员 3.修改出战成员 4. 查看出战成员 5.查看军团成员");
+			choice=read.nextInt();
+		}
+		read.close();
+		System.out.println("退出成功!");
 	}
 	public void Init_Army(Army[] arr_Army) {
 		arr_Army[0]=new Army(0,"锐雯","战士",6300,true);
