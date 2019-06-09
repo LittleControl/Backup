6df271bbe729aaed1095e1eb484b8f2a82e90d52
LittleControl <littlecontrol@qq.com>
2019-06-09 17:36:28 +0800

    手写字符串数组的自然排序

--- Code/Java/new28/src/top/littlecontrol/StrSort.java
+++ Code/Java/new28/src/top/littlecontrol/StrSort.java
@@ -5,12 +5,40 @@
 package top.littlecontrol;
 
 /*
- * 对字符串中的内容进行自然排序
+ * 对字符串数组中的内容进行自然排序
  *
  * */
 public class StrSort {
-    public String mySort(String str) {
-        char[] arr = str.toCharArray();
-        return "";
+    public static void main(String[] args) {
+        StrSort ss = new StrSort();
+        String[] str = new String[]{"EE", "DD", "AA", "CC", "BB"};
+        ss.mySort(str);
+        for (int i = 0; i < str.length; i++) {
+            System.out.println(str[i]);
+        }
+    }
+
+    /* 我我我我连冒泡排序都不会写了,(╯﹏╰) */
+    public String[] mySort(String[] str) {
+        for (int i = 0; i < str.length - 1; i++) {
+            for (int j = 0; j < str.length - 1 - i; j++) {
+                int isExchange = 0;
+                char[] arr1 = str[j].toCharArray();
+                char[] arr2 = str[j + 1].toCharArray();
+                int tail = Math.min(arr1.length, arr2.length);
+                for (int k = 0; k < tail; k++) {
+                    if (arr1[k] > arr2[k]) {
+                        isExchange = 1;
+                        break;
+                    }
+                }
+                if (isExchange == 1) {
+                    String temp = str[j];
+                    str[j] = str[j + 1];
+                    str[j + 1] = temp;
+                }
+            }
+        }
+        return str;
     }
 }
