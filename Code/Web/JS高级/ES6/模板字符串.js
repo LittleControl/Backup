c9762b08cacf271b94d4c3886f65417030df9929
LittleControl <littlecontrol@qq.com>
2019-09-05 10:22:04 +0800

    模板字符串

---
+++ Code/Web/JS高级/ES6/模板字符串.js
@@ -0,0 +1,8 @@
+let obj  ={
+    name:'Little',
+    age:18
+}
+let str1 = 'My name is ' + obj.name + ' and my age is ' + obj.age
+let str2 = `My name is ${obj.name} and my age is ${obj.age}`
+console.log(str1)
+console.log(str2)
