0198f1b0e33f0c1a8478f7c7984deed3a28d99ef
LittleControl <littlecontrol@qq.com>
2019-09-07 20:33:13 +0800

    字符串的常用方法

---
+++ Code/Web/JS高级/ES6/拓展方法/字符串常用方法.js
@@ -0,0 +1,6 @@
+let str = 'Little'
+console.log(str.startsWith('A'))
+console.log(str.startsWith('L'))
+console.log(str.endsWith('e'))
+console.log(str.includes('Little'))
+console.log(str.repeat(3))
