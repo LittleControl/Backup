6ee0f4db3afa907a8cdaf67c3ad5fb3a2bbc065c
LittleControl <littlecontrol@qq.com>
2019-09-07 20:33:37 +0800

    数值的拓展方法

---
+++ Code/Web/JS高级/ES6/拓展方法/数值的拓展方法.js
@@ -0,0 +1,12 @@
+/* 在JS中用0b表示二进制数,用0o表示八进制数 */
+console.log(0b10010)
+console.log(0o130)
+let num = 3
+console.log(Number.isFinite(num))
+console.log(Number.isFinite(Infinity))
+console.log(Number.isInteger(12.3))
+console.log(Number.isInteger(12.0))
+console.log(Number.parseInt('123abc'))
+console.log(Number.parseInt('abc123'))
+console.log(Number.parseInt('a'))
+console.log(Math.trunc(12.33))//直接去除小数部分
