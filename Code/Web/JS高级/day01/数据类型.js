997f6db0f5bfeccd7358e24d75130afcde5132d1
LittleControl <littlecontrol@qq.com>
2019-09-09 21:06:58 +0800

    格式优化

--- Code/Web/JS高级/day01/数据类型.js
+++ Code/Web/JS高级/day01/数据类型.js
@@ -1,20 +1,20 @@
-var i=3
+var i = 3
 console.log(typeof i);
 console.log(i instanceof Number);
-console.log(i===3);
-var f=function(){
+console.log(i === 3);
+var f = function () {
     console.log("I'm a function");
 }
 console.log(f);
 console.log(typeof f);
 var a;
 console.log(a);
-var b=null;
+var b = null;
 console.log(typeof b);
-var c=[];
-var d={};
-var e="";
-console.log(typeof c,typeof b,typeof d,typeof e);
+var c = [];
+var d = {};
+var e = "";
+console.log(typeof c, typeof b, typeof d, typeof e);
 console.log(c instanceof Array);
 console.log(c instanceof Object);
 console.log(d instanceof Object);
