3ee36e59b79659af344548f06124603c0c9fc5fd
LittleControl <littlecontrol@qq.com>
2019-09-29 17:07:22 +0800

    输出结果显示优化

--- Code/Web/JS高级/JS模块化/ES6/js/libs/module3.js
+++ Code/Web/JS高级/JS模块化/ES6/js/libs/module3.js
@@ -6,10 +6,10 @@ Object.defineProperty(exports, "__esModule", {
 /* 默认暴露 */
 
 var fun5 = function fun5() {
-    console.log('fun4()... module3');
+    console.log('fun5()... module3');
 };
 var fun6 = function fun6() {
-    console.log('fun5()... module3');
+    console.log('fun6()... module3');
 };
 
 exports.default = { fun6: fun6, fun5: fun5 };
